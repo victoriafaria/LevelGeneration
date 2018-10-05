a4dfe9e8ab43bb1765841806582c61b2043343df
Victoria Andressa Faria <victoriaamdf@gmail.com>
2018-10-05 00:19:02 -0300

    Firth Commit

---
+++ LevelGeneration/Assets/LevelBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour {
+
+    public TextAsset m_File;
+
+    public List<LevelObject> m_Objects;
+
+    public void Start() {
+
+    
+        string[] lines = m_File.text.Replace("\r", "").Split('\n');
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                char symbol = lines[i][j];
+                for (int k = 0; k < m_Objects.Count; k++)
+                {
+                    if (symbol == m_Objects[k].m_Symbol) {
+
+                        Vector3 position = new Vector3(j, -i, 0.0f);
+                        GameObject go = Instantiate(m_Objects[k].m_Object, position, Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ LevelGeneration/Assets/LevelObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class LevelObject {
+
+    public char m_Symbol;
+
+    public GameObject m_Object;
+	
+}
